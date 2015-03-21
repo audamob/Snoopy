436713932fd064f8aa2b322bc6520b124c1bf6d7
houssemSoat <houssem.benslama@gmail.com>
2015-03-21 02:09:29 +0100

    This the first commit of Snoopy project

---
+++ Snoopy/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace Snoopy.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}

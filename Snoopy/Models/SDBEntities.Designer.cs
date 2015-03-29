37658b2cf178dc6cda9ea923c1f8058d205ee6d1
houssemSoat <houssem.benslama@gmail.com>
2015-03-30 01:04:33 +0200

    Commit Models

---
+++ Snoopy/Models/SDBEntities.Designer.cs
@@ -0,0 +1,4 @@
+﻿// La génération de code par défaut est désactivée pour le modèle 'C:\Users\Aymen\Documents\Visual Studio 2012\Audamob Projects\Snoopy\Snoopy\Models\SDBEntities.edmx'. 
+// Pour l'activer, modifiez la valeur du concepteur 'Stratégie de génération de code'.
+// Cette propriété est disponible dans la fenêtre Propriétés, lorsque le modèle est ouvert
+// dans le concepteur.
